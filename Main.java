55b150c6967e02fc9470fd5c608af3499a0c5014
vikashprajapati <vikash.prajapati@nagarro.com>
2023-09-12 08:45:00 +0530

    Added Data class

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is a JAVA project which is executing through Jenkins");
         System.out.println("Hello Vikash Kumar Prajapati");
+        System.out.println(new Date());
     }
 }
