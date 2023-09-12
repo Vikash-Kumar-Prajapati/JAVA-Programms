1d3e1f8b512a8dc49f21ca6b34f80a9e1b458e34
vikashprajapati <vikash.prajapati@nagarro.com>
2023-09-12 08:30:06 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a JAVA project which is executing through Jenkins");
+        System.out.println("Hello Vikash Kumar Prajapati");
+    }
+}
